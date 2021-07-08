371d1d9c0e648e887ca0a68a6f2ee943605ee0ef
swapnanil  mukherjee <swapnanil.mukherjee12@gmail.com>
2021-07-08 21:44:54 +0530

    Section 4 Task 1
    created and deployed first chaipy bot

---
+++ ChaiBot/uploader.py
@@ -0,0 +1,34 @@
+from chai_py import Metadata, package, upload_and_deploy, wait_for_deployment
+from chai_py import share_bot
+from chai_py.auth import set_auth
+
+from starter_bot import Bot
+
+from chai_py.defaults import GUEST_UID, GUEST_KEY
+
+DEVELOPER_UID = 'fMVOY9mim0aoY1feazbmuWejkXU2'
+DEVELOPER_KEY = 'Ixz2-_zxnkWh4uMiKRDCgKjH0VLIh344znM-vlcRkoDpzS3Au7NKstzl1bVh38o1bOt7TZiHkukGuHMzEHg4VQ'
+
+if DEVELOPER_KEY is None or DEVELOPER_UID is None:
+    raise RuntimeError("Please fetch your UID and KEY from the bottom of the Chai Developer Platform. https://chai.ml/dev")
+
+set_auth(DEVELOPER_UID, DEVELOPER_KEY)
+BOT_IMAGE_URL = "https://cutt.ly/lx0gnM9"
+
+package(
+    Metadata(
+        name="Your First Bot! 🎉 🤖",
+        image_url=BOT_IMAGE_URL,
+        color="f1a2b3",
+        description="Thank you for creating me ❤️",
+        input_class=Bot,
+     )
+)
+
+bot_uid = upload_and_deploy(
+    "_package.zip"
+)
+
+wait_for_deployment(bot_uid)
+
+share_bot(bot_uid)
